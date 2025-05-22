272df701f5628e1c5ccc860a9dfbab4a5a2b4a82
SiriusXT <1160925501@qq.com>
2025-05-22 15:33:11 +0800

    Remove deprecated

--- Ctrl+F Calling trilium's ow.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('keydown', function (event) {
-    if (event.ctrlKey && event.key.toLowerCase() === 'f') {
-        event.preventDefault(); 
-        event.stopPropagation(); 
-        api.triggerEvent('findInText');
-    }
-}, true); 
